49019d2eeddf0f3af70b6d4ac0d3a8d66f000cc9
NME.eth <nme@ethmail.cc>
2022-02-18 00:29:01 +0200

    Made bot variable names genearl and not specific to any token

--- src/bot.js
+++ src/bot.js
@@ -7,6 +7,7 @@ const MNEMONIC = process.env.MNEMONIC;
 const NODE_URL = process.env.NODE_URL;
 const BUY_AMT = process.env.BUY_AMT;
 const MIN_LIQUIDITY = process.env.MIN_LIQUIDITY;
+const TOKEN_OUT_NAME = process.env.TOKEN_OUT_NAME;
 
 // setting up node provider and account wallet for signing
 const provider = new ethers.providers.JsonRpcProvider(NODE_URL);
@@ -15,12 +16,12 @@ const signer = wallet.connect(provider);
 
 const addresses = {
   receiver: signer.address,  
-  wETH: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
-  router: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff',  
-  factory: '0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32', 
+  tokenIn: process.env.TOKEN_IN_ADDR,
+  router: process.env.ROUTER_ADDR,
+  factory: process.env.FACTORY_ADDR, 
 }
 
-// instantiating Quickswap contracts
+// instantiating our contracts
 const factory = new ethers.Contract(
   addresses.factory,
   abi['factory'],
@@ -31,6 +32,11 @@ const router = new ethers.Contract(
   abi['router'],
   signer
 );
+const tokenInContract = new ethers.Contract(
+  addresses.tokenIn,
+  abi['erc20'],
+  signer
+);
 
 console.log('Signer Address : ', signer.address);
 console.log('- - - - - ');
@@ -38,6 +44,10 @@ console.log('Waiting for new pairs');
 
 // checking if the factory contract emits the PairCreated event
 factory.on('PairCreated', async (token0, token1, pairAddress) => {
+
+  // setting tokenIn's name
+  tokenInSymbol = await tokenInContract.symbol();
+
   console.log(`
     New pair detected
     =================
@@ -47,76 +57,65 @@ factory.on('PairCreated', async (token0, token1, pairAddress) => {
   `);
 
   // finding the other pair token's address
-  // inTokenAddr will always equal wETH's addr
-  // outTokenAddr will equal the other token pair's addr
-  let inTokenAddr, outTokenAddr;
-  if(token0 === addresses.wETH) {
-    inTokenAddr = token0; 
-    outTokenAddr = token1;
+  // tokenIn is the token address we are sending to router (token we are selling- deposit to liquidity pool)
+  // tokenOut is the token address we are receive from trade (the token we are buying)
+  let tokenIn, tokenOut;
+  if(token0 === addresses.tokenIn) {
+    tokenIn = token0; 
+    tokenOut = token1;
   }
-  if(token1 == addresses.wETH) {
-    inTokenAddr = token1; 
-    outTokenAddr = token0;
+  if(token1 == addresses.tokenIn) {
+    tokenIn = token1; 
+    tokenOut = token0;
   }
 
-  if(typeof inTokenAddr === 'undefined') {
-    console.log("not a wETH pair (ignoring it)");
+  if(typeof tokenIn === 'undefined') {
+    console.log(`not a ${tokenInSymbol} pair (ignoring it)`);
     return;
   }
 
-  // extracting outToken's symbol to check if it equals 'milk'
-  const outTokenContract = new ethers.Contract(outTokenAddr, abi['erc20'], provider);
-  let outTokenSymbol = await outTokenContract.symbol();
+  // extracting outToken's symbol to check if it equals the token we are looking for
+  const tokenOutContract = new ethers.Contract(tokenOut, abi['erc20'], provider);
+  let outTokenSymbol = await tokenOutContract.symbol();
 
-  if(outTokenSymbol.toLowerCase() === "milk") {
-    console.log("Found Milk Contract Addr... Now Waiting For Liquidity To Be Added"); 
-    // renaming variables now that we confirm outToken is Milk 
-    wethAddr = inTokenAddr;
-    milkAddr = outTokenAddr;
+  if(outTokenSymbol.toLowerCase() === TOKEN_OUT_NAME.toLowerCase()) {
+    console.log(`Found ${TOKEN_OUT_NAME}'s Contract Addr... Now Waiting For Liquidity To Be Added`); 
 
-    // finding weth's token index in pair contract (either 0 or 1)
-    // 0 means weth addr < milk addr
-    // 1 means weth addr > milk addr
-    let wethIndex = (wethAddr.toLowerCase() < milkAddr.toLowerCase()) ? 0 : 1;
+    // finding tokenIn's index in pair contrat (either 0 or 1)
+    // 0 means tokenIn addr < tokenOut addr
+    // 1 means tokenIn addr > tokenOut addr
+    let tokenInIndex = (tokenIn.toLowerCase() < tokenOut.toLowerCase()) ? 0 : 1;
 
-    // waiting for liquidity to be added to the new weth-milk pair
+    // waiting for liquidity to be added to the new the pair we found
     var timer = setInterval(async () => {
       const pairContract = new ethers.Contract(pairAddress, abi['pair'], provider);
       const rawReserves = await pairContract.getReserves();
-      const wethReserves = ethers.utils.formatEther(ethers.BigNumber.from(rawReserves[wethIndex]));
+      const tokenInReserves = ethers.utils.formatEther(ethers.BigNumber.from(rawReserves[tokenInIndex]));
 
-      console.log("wETH reserves : ", wethReserves);
+      console.log(`${tokenInSymbol} reserves : ${tokenInReserves}`);
       
-      // liquidity has been added once the pairContract weth reserves >= 100weth
-      if(wethReserves >= MIN_LIQUIDITY) {
+      // liquidity has been added once the pairContract tokenIn reserves >= MIN_LIQUIDITY
+      if(tokenInReserves >= MIN_LIQUIDITY) {
         clearInterval(timer);
         console.log("\nSufficient Liquidity Has Been Added To Pool\n");
 
-        // creating buy transaction
+        // creating receive transaction
         const amountIn = ethers.utils.parseUnits(BUY_AMT, 'ether');
-        const amounts = await router.getAmountsOut(amountIn, [wethAddr, milkAddr]);
+        const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
         const amountOutMin = amounts[1].sub(amounts[1].div(30)); // accept swap as long as change in amt received <30% 
-        //console.log(`
-        // Buying new token
-        // =================
-        // wethAddr: ${ethers.utils.formatEther(amountIn.toString())} 
-        // milkAddr: ${ethers.utils.formatEther(amountOutMin.toString())}
-        //`);
-
-        // approving the router to transfer our Weth.
+        
+        // approving the router to transfer our tokenIn.
         // this is normally done before running this bot to reduce latency but
         // for the sake of simplicity I am including it here 
-        const wethContract = new ethers.Contract(wethAddr, abi['erc20'], signer);
-       	const approve_weth_tx = await wethContract.approve(addresses.router, "115792089237316195423570985008687907853269984665640564039457584007913129639935");
-        const approve_weth_promise = approve_weth_tx.wait();
-        const approve_weth_receipt = await approve_weth_promise;
-        //console.log(`approved router to use weth - (tx:${approve_weth_receipt.transactionHash})`);
+       	const approve_tokenIn_tx = await tokenInContract.approve(addresses.router, "115792089237316195423570985008687907853269984665640564039457584007913129639935");
+        const approve_tokenIn_promise = approve_tokenIn_tx.wait();
+        const approve_tokenIn_receipt = await approve_tokenIn_promise;
  
         // sending our transaction
         const tx = await router.swapExactTokensForTokens(
           amountIn,
           amountOutMin,
-          [wethAddr, milkAddr],
+          [tokenIn, tokenOut],
           addresses.receiver,
           Date.now() + 1000 * 60 * 1, 
           //{
@@ -133,8 +132,8 @@ factory.on('PairCreated', async (token0, token1, pairAddress) => {
         console.log("= ".repeat(12));
         console.log("[Swap Succesfully Made]")
         console.log("= ".repeat(12)+"\n");
-        console.log(`sent \t\t: ${ethers.utils.formatEther(amountIn.toString())} weth`);
-        console.log(`received\t: ${ethers.utils.formatEther(await outTokenContract.balanceOf(signer.address))} milk`);
+        console.log(`sent \t\t: ${ethers.utils.formatEther(amountIn.toString())} ${tokenInSymbol}`);
+        console.log(`received\t: ${ethers.utils.formatEther(await tokenOutContract.balanceOf(signer.address))} ${TOKEN_OUT_NAME}`);
         console.log("- ".repeat(12));
         console.log(`(tx:${receipt.transactionHash})`);
       } 
